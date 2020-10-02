0a81d1b761503607cca2dcaf48a3c3c2035ef182
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-10-02 12:17:47 -0300

    Subindo funcionalidade cadastrar

---
+++ blogPessoal/src/app/service/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(usuarioLogin: UsuarioLogin) {
+    return this.http.post('http://localhost:8080/usuarios/logar', usuarioLogin)
+  }
+
+  cadastrar(usuario: Usuario){
+    return this.http.post('http://localhost:8080/usuarios/cadastrar',usuario)
+  }
+
+}
