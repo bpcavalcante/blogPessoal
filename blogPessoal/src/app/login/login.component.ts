7e633533dbd09b575dc6af20059b034181a518da
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-10-02 13:21:36 -0300

    Subindo função de login

--- blogPessoal/src/app/login/login.component.ts
+++ blogPessoal/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+
+  }
+
+  entrar(){
+    this.authService.logar(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
+      this.usuarioLogin = resp
+      localStorage.setItem('token', this.usuarioLogin.token)
+      this.router.navigate(['/feed'])
+    })
   }
 
 }
