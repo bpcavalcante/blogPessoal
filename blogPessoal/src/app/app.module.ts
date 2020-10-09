178df578c9016f64bb432406e35692b8d262afda
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-10-09 13:23:57 -0300

    Terminando o Projeto

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PutTemaComponent } from './put-tema/put-tema.component';
 import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
 import { AlertasComponent } from './alertas/alertas.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 @NgModule({
@@ -50,7 +51,12 @@ import { AlertasComponent } from './alertas/alertas.component';
     OrderModule,
     ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
